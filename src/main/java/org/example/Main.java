60a5d6fcd1a1a0ff50e91cfe30911803c9dde4b1
Siphesihle-Thomo <seansthomo@gmail.com>
2024-09-29 21:14:47 +0200

    FEAT: use text block

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Welcome to the Number Guessing Game!\n" +
-                "I'm thinking of a number between 1 and 100.\n" +
-                "You have 5 chances to guess the correct number.\n" +
-                "\n" +
-                "Please select the difficulty level:\n" +
-                "1. Easy (10 chances)\n" +
-                "2. Medium (5 chances)\n" +
-                "3. Hard (3 chances)\n" +
-                "\n" +
-                "Enter your choice: ");
+        System.out.print("""
+                Welcome to the Number Guessing Game!
+                I'm thinking of a number between 1 and 100.
+                You have 5 chances to guess the correct number.
+                
+                Please select the difficulty level:
+                1. Easy (10 chances)
+                2. Medium (5 chances)
+                3. Hard (3 chances)
+                
+                Enter your choice: \s""");
 
         Scanner input = new Scanner(System.in);
         String difficulty = input.nextLine();
